c105a42bdf963cc77e6ab8515e10588d1a8fab65
Gabriel <gabrielmora2001@hotmail.com>
2021-09-23 12:35:57 -0500

    Se dibuja el elipse

--- src/main/Main.java
+++ src/main/Main.java
@@ -21,82 +21,122 @@ public class Main extends PApplet {
 	private Socket socketcito;
 	private BufferedWriter escritorcito;
 	private BufferedReader lectorcito;
-	
+	private int posX;
+	private int posY;
+	private int r, g, b;
+	private String direc;
+	private boolean isActive;
 
 	public static void main(String[] args) {
 		PApplet.main("main.Main");
 
 	}
-	public void settings(){
-		size(500,500);
+
+	public void settings() {
+		size(500, 500);
 	}
-		public void setup(){
-			background(100,0,0);
-			initServer();
-		}
-		public void draw(){
-			background(0);
-				fill(255,0,0);
-				ellipse(10,10,20,20);
+
+	public void setup() {
+		background(100, 0, 0);
+		initServer();
+		r = 255;
+		g = 0;
+		b = 0;
+		posX = 250;
+		posY = 250;
+		direc="";
+	}
+
+	public void draw() {
+		background(0);
+		switch (direc) {
+		case "UP":
+if(isActive) {
+	posY--;
+}
+			break;
+
+		case "DOWN":
+			if(isActive) {
+				posY++;
 			}
-		
-		public void initServer() {
-			new Thread(
-					
-					()->{
-						try {
-							//Paso 1: Esperar una conexion	
-							ServerSocket server = new ServerSocket(6969);
-							System.out.println("Esperando conexión....");
-							socketcito = server.accept();
-							//Paso 3: Cliente y Server conectados
-							System.out.println("Cliente conectado!!!");
-							
-							InputStream is = socketcito.getInputStream();
-							InputStreamReader isr = new InputStreamReader(is);
-							lectorcito = new BufferedReader(isr);
-							
-							OutputStream os = socketcito.getOutputStream();
-							OutputStreamWriter osw = new OutputStreamWriter(os);
-							escritorcito = new BufferedWriter(osw);
-							//Recibir mensajitos
-							
-							while(true) {
+
+			break;
+		case "LEFT":
+			if(isActive) {
+				posX--;
+			}
+			break;
+		case "RIGHT":
+			if(isActive) {
+				posX++;
+			}
+			break;
+		case "COLOR":
+			if(isActive) {
+			r= (int)random(0,255);
+			g= (int)random(0,255);
+			b= (int)random(0,255);
+			}
+			break;
+			default:
+				
+		}
+		fill(r, g, b);
+		ellipse(posX, posY, 100, 100);
+	}
+
+	public void initServer() {
+		new Thread(
+
+				() -> {
+					try {
+						// Paso 1: Esperar una conexion
+						ServerSocket server = new ServerSocket(6969);
+						System.out.println("Esperando conexión....");
+						socketcito = server.accept();
+						// Paso 3: Cliente y Server conectados
+						System.out.println("Cliente conectado!!!");
+
+						InputStream is = socketcito.getInputStream();
+						InputStreamReader isr = new InputStreamReader(is);
+						lectorcito = new BufferedReader(isr);
+
+						OutputStream os = socketcito.getOutputStream();
+						OutputStreamWriter osw = new OutputStreamWriter(os);
+						escritorcito = new BufferedWriter(osw);
+						// Recibir mensajitos
+
+						while (true) {
 							System.out.println("Esperando mensaje....");
 							String line = lectorcito.readLine();
-							//System.out.println("Recibido: " + line);
+							// System.out.println("Recibido: " + line);
 							Gson gson = new Gson();
 							Instruction je = gson.fromJson(line, Instruction.class);
-							System.out.println(je.getKey());
-							System.out.println(je.isActive());
-							
-							}
-							
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
+							direc = je.getKey();
+							isActive = je.isActive();
+
 						}
+
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
 					}
-					
-					
-					
-					).start();
-		}
-		public void sendMessage(String msg) {
-			new Thread(
-					()->{
-						try {
-							escritorcito.write(msg+"\n");
-							escritorcito.flush();
-							
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-					).start();
-		}
+				}
+
+		).start();
 	}
 
+	public void sendMessage(String msg) {
+		new Thread(() -> {
+			try {
+				escritorcito.write(msg + "\n");
+				escritorcito.flush();
 
-
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}).start();
+	}
+}
